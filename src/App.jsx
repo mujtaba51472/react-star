46c7d36e836c886d5dabd91ec4c429f44268e40f
mujtaba514 <mujtaba51472@gmail.com>
2023-03-27 03:11:53 +0500

    add hero section in home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import React from 'react'
+import {BrowserRouter as Router , Routes , Route} from 'react-router-dom'
+import Header from './components/Header'
+import Home from './components/Home'
+
+import './styles/app.scss'
+import './styles/header.scss'
+import './styles/home.scss'
 
 
-const  App=()=>  (
-    <div className="App">
-     
-    </div>
-  );
+const App = () => {
+  return (
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </Router>
+  )
+}
 
-
-export default App;
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <main>
+        <h1>Awsome coding</h1>
+        <p>Solutions to all your problems</p>
+        </main>
+      
+
+    </div>
+  )
+}
+
+export default Home
